48772d4768688f2d323af43bbf9b035d2daa2abe
Aleksey Shagraev <alex-shagraev@ya.ru>
2018-07-21 02:15:56 +0300

    better

--- src/Solution/Program.cs
+++ src/Solution/Program.cs
@@ -8,13 +8,20 @@
         private static void Main(string[] args)
         {
             var state = new TState();
-            state.Load("problems/LA001_tgt.mdl");
+            state.Load("problems/LA130_tgt.mdl");
 
-            List<object> commands = TraceReader.Read("traces/LA001.nbt");
+            List<object> commands = TraceReader.Read("traces/LA130.nbt");
 
+            int step = 0;
             while (commands.Count > 0)
             {
                 state.ApplyCommands(commands);
+                step += 1;
+
+                if (step % 10000 == 0)
+                {
+                    Console.WriteLine(step);
+                }
             }
 
             Console.WriteLine(state.HasValidFinalState());
