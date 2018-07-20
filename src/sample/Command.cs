93211354611e3fcf081c7c70733e5d215e78ca27
Ivan Komarov <Ivan.Komarov@dfyz.info>
2018-07-20 23:47:14 +0300

    Commands.

---
+++ src/sample/Command.cs
@@ -0,0 +1,48 @@
+﻿namespace Sample
+{
+    public class CoordDiff
+    {
+        public int Dx;
+        public int Dy;
+        public int Dz;
+    }
+
+    public class Halt
+    {
+    }
+
+    public class Wait
+    {
+    }
+
+    public class Flip
+    {
+    }
+
+    public class StraightMove
+    {
+        public CoordDiff Diff;
+    }
+
+    public class LMove
+    {
+        public CoordDiff Diff1;
+        public CoordDiff Diff2;
+    }
+
+    public class Fission
+    {
+        public CoordDiff Diff;
+        public int M;
+    }
+
+    public class Fill
+    {
+        public CoordDiff Diff;
+    }
+
+    public class Fusion
+    {
+        public CoordDiff Diff;
+    }
+}
