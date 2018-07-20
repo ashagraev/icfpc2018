89da3124b3a134a3c8e9fcc52fb5920b15f5c77c
Ivan Komarov <Ivan.Komarov@dfyz.info>
2018-07-21 01:47:18 +0300

    Console trace reader.

---
+++ TraceReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TraceReaderTool
+{
+    using System.IO;
+
+    using Solution;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: TRACE_FILE");
+                Environment.Exit(1);
+            }
+            var bytes = File.ReadAllBytes(args[0]);
+            var commands = TraceReader.Read(bytes);
+            foreach (var cmd in commands)
+            {
+                Console.WriteLine(cmd);
+            }
+        }
+    }
+}
--- src/Solution/Command.cs
+++ src/Solution/Command.cs
@@ -9,49 +9,66 @@
         public int Dz;
 
         public int MLen() => Math.Abs(Dx) + Math.Abs(Dy) + Math.Abs(Dz);
+
+        public override string ToString() => string.Format("<dx={0}, dy={1}, dz={2}>", Dx, Dy, Dz);
     }
 
     public class Halt
     {
+        public override string ToString() => "Halt";
     }
 
     public class Wait
     {
+        public override string ToString() => "Wait";
     }
 
     public class Flip
     {
+        public override string ToString() => "Flip";
     }
 
     public class StraightMove
     {
         public CoordDiff Diff;
+
+        public override string ToString() => string.Format("SMove(d={0})", Diff);
     }
 
     public class LMove
     {
         public CoordDiff Diff1;
         public CoordDiff Diff2;
+
+        public override string ToString() => string.Format("LMove(d1={0}, d2={1})", Diff1, Diff2);
     }
 
     public class Fission
     {
         public CoordDiff Diff;
         public int M;
+
+        public override string ToString() => string.Format("Fission(d={0}, m={1})", Diff, M);
     }
 
     public class Fill
     {
         public CoordDiff Diff;
+
+        public override string ToString() => string.Format("Fill(d={0})", Diff);
     }
 
     public class FusionP
     {
         public CoordDiff Diff;
+
+        public override string ToString() => string.Format("FusionP(d={0})", Diff);
     }
 
     public class FusionS
     {
         public CoordDiff Diff;
+
+        public override string ToString() => string.Format("FusionS(d={0})", Diff);
     }
 }
