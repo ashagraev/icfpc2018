1fa2072e0292210f4b6c6c7d94cc132cc6ffeb2b
Aleksey Shagraev <alex-shagraev@ya.ru>
2018-07-21 00:00:16 +0300

    state

--- src/sample/Program.cs
+++ src/sample/Program.cs
@@ -1,55 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using sample;
 
 namespace Sample
 {
-    internal class TMdl
-    {
-        public int[,,] Matrix;
-        public byte R;
-
-        public TMdl()
-        {
-        }
-
-        public TMdl(string path)
-        {
-            var result = new TMdl();
-
-            var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
-            var br = new BinaryReader(fs, new ASCIIEncoding());
-
-            int R = br.ReadByte();
-            Matrix = new int[R, R, R];
-
-            var bytesCount = (int) Math.Ceiling((float) Matrix.Length / 8);
-            var bytes = br.ReadBytes(bytesCount);
-
-            for (var x = 0; x < R; ++x)
-            for (var y = 0; y < R; ++y)
-            for (var z = 0; z < R; ++z)
-            {
-                var bitNumber = x * R * R + y * R + z;
-
-                var byteNumber = bitNumber / 8;
-
-                var shift = bitNumber % 8;
-
-                var mask = 1 << shift;
-                int curByte = bytes[byteNumber];
-                var curRes = curByte & mask;
-
-                Matrix[x, y, z] = curRes > 0 ? 1 : 0;
-            }
-        }
-    }
-
     internal class Program
     {
         private static void Main(string[] args)
         {
-            var mdl = new TMdl("problems/LA001_tgt.mdl");
+            TState state = new TState();
+            state.Load("problems/LA001_tgt.mdl");
         }
     }
 }
---
+++ src/sample/State.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace sample
+{
+    enum EHarmonics
+    {
+        High,
+        Low
+    }
+
+    internal class TCoord
+    {
+        public int X;
+        public int Y;
+        public int Z;
+    }
+
+    internal class TBot
+    {
+        public int Bid;
+        public TCoord Coord;
+        public int[] Seeds;
+    }
+
+    internal class TState
+    {
+        public byte R;
+
+        public int Energy = 0;
+        public EHarmonics Harmonics = EHarmonics.Low;
+
+        public int[,,] Matrix;
+        public TBot[] Bots;
+        public object[] Commands;
+
+        public void Load(string path)
+        {
+            var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var br = new BinaryReader(fs, new ASCIIEncoding());
+
+            int R = br.ReadByte();
+            Matrix = new int[R, R, R];
+
+            var bytesCount = (int) Math.Ceiling((float) Matrix.Length / 8);
+            var bytes = br.ReadBytes(bytesCount);
+
+            for (var x = 0; x < R; ++x)
+            for (var y = 0; y < R; ++y)
+            for (var z = 0; z < R; ++z)
+            {
+                var bitNumber = x * R * R + y * R + z;
+
+                var byteNumber = bitNumber / 8;
+
+                var shift = bitNumber % 8;
+
+                var mask = 1 << shift;
+                int curByte = bytes[byteNumber];
+                var curRes = curByte & mask;
+
+                Matrix[x, y, z] = curRes > 0 ? 1 : 0;
+            }
+        }
+    }
+}
